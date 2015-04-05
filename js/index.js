9cf1cedf29da8a59a3cf4300e0526a4b8053daf5
Tkachenko Sergey <ts1503@yandex-team.ru>
2015-04-05 09:51:12 +0300

    add typehead

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
 
-document.addEventListener('DOMContentLoaded', function () {
-    var currentTab = document.querySelector('#currentUrl');
+$(document).ready(function () {
+    var currentTab = $('#currentUrl');
 
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         var tabUrl;
@@ -11,6 +21,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
         tabUrl = tabs[0].url;
 
-        currentTab.innerText = tabUrl;
+        currentTab.text(tabUrl);
+        initInput();
     });
 });
+
+function initInput () {
+    $('#searchInput').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    },
+    {
+    name: 'states',
+        displayKey: 'value',
+        source: substringMatcher(states)
+    });
+}
+
+function substringMatcher (strs) {
+    return function findMatches(q, cb) {
+        var matches, substrRegex;
+
+        matches = [];
+
+        substrRegex = new RegExp(q, 'i');
+
+        $.each(strs, function(i, str) {
+            if (substrRegex.test(str)) {
+                matches.push({ value: str });
+            }
+        });
+
+        cb(matches);
+    };
+}
